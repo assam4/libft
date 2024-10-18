2a5dd6320509275f7f55d21b4c6186f423e10273
Samvel <115891697+assam4@users.noreply.github.com>
2024-10-18 16:02:36 +0400

    Add files via upload

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*core;
+	t_list	*last;
+
+	core = NULL;
+	if (lst && lst->content && f && del)
+	{
+		core = ft_lstnew(f(lst->content));
+		if (!core || !core->content)
+			return (NULL);
+		lst = lst->next;
+		while (lst)
+		{
+			last = ft_lstlast(core);
+			last->next = ft_lstnew(f(lst->content));
+			if (!last->next || !last->next->content)
+			{
+				ft_lstclear(&core, del);
+				return (NULL);
+			}
+			lst = lst->next;
+		}
+	}
+	return (core);
+}
