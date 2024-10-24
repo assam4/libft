e3fa8914735aecdaf0b8bf82b89ae09ddfbb758c
Samvel <115891697+assam4@users.noreply.github.com>
2024-10-25 02:04:31 +0400

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,22 +6,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	if (!s1 && s2)
-		return (ft_strdup(s2));
-	else if (s1 && !s2)
-		return (ft_strdup(s1));
-	else if (s1 && s2)
-	{
-		s1_len = ft_strlen(s1);
-		s2_len = ft_strlen(s2);
-		ptr = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
-		if (ptr)
-		{
-			ft_strlcpy(ptr, s1, s1_len + 1);
-			ft_strlcpy(ptr + s1_len, s2, s2_len + 1);
-		}
-		return (ptr);
-	}
-	else
+	if (!s1 && !s2)
 		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	ptr = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (ptr)
+	{
+		ft_strlcpy(ptr, s1, s1_len + 1);
+		ft_strlcpy(ptr + s1_len, s2, s2_len + 1);
+	}
+	return (ptr);
+	}
 }
