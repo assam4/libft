05d805b9eff72ea764dd9964b6946687670102b2
Samvel <115891697+assam4@users.noreply.github.com>
2025-01-31 01:16:56 +0400

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,14 +14,9 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	s_len;
-	char	letter;
-
-	letter = (char)c;
-	s_len = ft_strlen(s) + 1;
-	while (s_len--)
+	while (s && *s)
 	{
-		if (*s == letter)
+		if (*s == c)
 			return ((char *)s);
 		++s;
 	}
